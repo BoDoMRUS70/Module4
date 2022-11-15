1f2bdc9da2df66eaefd6996c67c8723690080920
Bodomrus70 <Bodomrus70@mail.ru>
2022-11-16 01:13:58 +0700

    namespace Module4

--- Module 4/Module 4.cs
+++ Module 4/Module 4.cs
@@ -1,47 +1,32 @@
 ﻿using System;
 
-class MainClass
+namespace Module4
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Цикл do");
-        int t = 0;
-
-        do
+        static void Main(string[] args)
         {
-            Console.WriteLine(t);
+            int sum = 0;
 
-            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-            switch (Console.ReadLine())
+            while (true)
             {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Введите число");
+                var number = Convert.ToInt32(Console.ReadLine());
 
-                    Console.WriteLine("Your color is red!");
+                if (number < 0)
+                {
+                    continue;
+                }
+                else if (number == 0)
+                {
                     break;
+                }
 
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is green!");
-                    break;
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is cyan!");
-                    break;
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    Console.WriteLine("Your color is yellow!");
-                    break;
+                // number > 0
+                sum += number;
             }
 
-            t++;
-        } while (t < 3);
+            Console.WriteLine("Итоговая сумма: {0}", sum);
+        }
     }
 }
