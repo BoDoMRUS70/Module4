8adb7944766e021d93267a8fa90cbc1f7eca2640
Bodomrus70 <Bodomrus70@mail.ru>
2022-11-15 13:01:39 +0700

    Добавьте файлы проекта.

---
+++ Module 4/Module 4.cs
@@ -0,0 +1,12 @@
+﻿namespace Module4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool trueVar = true;
+            bool ivertedVar = !trueVar;
+            bool instVar = !ivertedVar;
+        }
+    }
+}
